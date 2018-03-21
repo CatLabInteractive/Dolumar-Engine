4c34a1a297aa13e0c2b35457888cf75fcff28d67
Thijs Van der Schaeghe <thijs@catlab.be>
2018-03-21 10:47:12 +0100

    Trying to fix the module loading issue.

--- src/Neuron/Core/ModuleFactory.php
+++ src/Neuron/Core/ModuleFactory.php
@@ -23,68 +23,72 @@
 
 abstract class Neuron_Core_ModuleFactory
 {
-	private $oModules = array ();
+    private $oModules = array();
 
-	/*
-		__get is run for reading data from inaccessible memebers
-	*/
-	public function __get ($sModule)
-	{
-		$this->doLoadModule ($sModule);
-		return $this->oModules[$sModule];
-	}
-	
-	public function moduleExists ($sModule)
-	{
-		$this->doLoadModule ($sModule);
-		if (!isset ($this->oModules[$sModule]))
-		{
-			print_r ($this);
-			return false;
-		}
-		else
-		{
-			return true;
-		}
-	}
-	
-	private function doLoadModule ($sModule)
-	{
-		if (!isset ($this->oModules))
-		{
-			$this->oModules = array ();
-		}
+    /**
+     * @param $sModule
+     * @return mixed
+     * @throws Neuron_Core_Error
+     */
+    public function __get($sModule)
+    {
+        $sModule = 'dynamicModule' . $sModule;
 
-		if (!isset ($this->oModules[$sModule]))
-		{
-			$this->oModules[$sModule] = $this->loadModule ($sModule);
-			
-			//var_dump ($this->oModules[$sModule]);
-			
-			if (!$this->oModules[$sModule])
-			{
-				throw new Neuron_Core_Error 
-				(
-					'ModuleFactory could not load module '.$sModule.' from class '.get_class ($this)
-				);
-			}
-		}
-	}
+        $this->doLoadModule($sModule);
+        return $this->oModules[$sModule];
+    }
 
-	protected abstract function loadModule ($sModule);
-	
-	public function __destruct ()
-	{
-		if (isset ($this->oModules) && is_array ($this->oModules))
-		{
-			foreach ($this->oModules as $k => $v)
-			{
-				//$v->__destruct ();
-				unset ($this->oModules[$k]);
-			}
-		}
-		
-		unset ($this->oModules);
-	}
+    /**
+     * @param $sModule
+     * @return bool
+     * @throws Neuron_Core_Error
+     */
+    public function moduleExists($sModule)
+    {
+        $sModule = 'dynamicModule' . $sModule;
+
+        $this->doLoadModule($sModule);
+        if (!isset ($this->oModules[$sModule])) {
+            print_r($this);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @param $sModule
+     * @throws Neuron_Core_Error
+     */
+    private function doLoadModule($sModule)
+    {
+        if (!isset ($this->oModules)) {
+            $this->oModules = array();
+        }
+
+        if (!isset ($this->oModules[$sModule])) {
+            $this->oModules[$sModule] = $this->loadModule($sModule);
+        }
+
+        if (!$this->oModules[$sModule]) {
+            throw new Neuron_Core_Error('ModuleFactory could not load module ' . $sModule . ' from class ' . get_class($this));
+        }
+    }
+
+    protected abstract function loadModule($sModule);
+
+    /**
+     *
+     */
+    public function __destruct()
+    {
+        if (isset ($this->oModules) && is_array($this->oModules)) {
+            foreach ($this->oModules as $k => $v) {
+                //$v->__destruct ();
+                unset ($this->oModules[$k]);
+            }
+        }
+
+        $this->oModules = array();
+    }
 }
-?>
