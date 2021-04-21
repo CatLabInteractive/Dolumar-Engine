ef1fbe9d79c46501a64dd562b3a9843a8ba438a4
Thijs Van der Schaeghe <thijs@catlab.be>
2021-04-21 10:24:18 +0200

    Messing with openid connect login to be able to switch to new authentication server.

--- src/Neuron/GameServer/scripts/openid2/redirect.php
+++ src/Neuron/GameServer/scripts/openid2/redirect.php
@@ -2,3 +2,20 @@
 
 $returnUrl = Neuron_URLBuilder::getInstance()->getRawURL('oauth2/login/next', []);
 
+$config = [
+    'client_id' => OPENID_CONNECT_CLIENT_ID,
+    'redirect_uri' => $returnUrl,
+    'authorization_endpoint' => OPENID_CONNECT_AUTHORIZE_URL,
+    'token_endpoint' => OPENID_CONNECT_TOKEN_URL,
+    'authentication_info' => [
+        'method' => 'client_secret_post',
+        'params' => array(
+            'client_secret' => OPENID_CONNECT_SECRET
+        )
+    ]
+];
+
+$flow = new \InoOicClient\Flow\Basic(['client_info' => $config ]);
+
+$url = $flow->getAuthorizationRequestUri('openid email profile');
+header('Location: ' . $url);
